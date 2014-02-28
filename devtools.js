41f49a42f76f08f96329455ea88590557d6cbd1a
Will Klein <wklein@rallydev.com>
2014-02-28 16:35:39 -0700

    Add Client Metrics panel.

--- devtools.js
+++ devtools.js
@@ -3,4 +3,5 @@
 // chrome.extension.*
 
 // Create a tab in the devtools area
-chrome.devtools.panels.create("Rally", "rally-icon-40.png", "panel.html", function(panel) {});
+chrome.devtools.panels.create("Boilerplate", "rally-icon-40.png", "panels/panel.html", function(panel) {});
+chrome.devtools.panels.create("Client Metrics", "rally-icon-40.png", "panels/clientmetrics.html", function(panel) {});
